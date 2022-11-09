309f9062ce06a9c2e0f73d17117f00b567ed8560
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2022-11-09 18:30:29 +0545

    [ADDED]: -added migration files for product and product categroy table

---
+++ database/migrations/2022_11_09_110720_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('product_name_nep');
+            $table->string('product_name_eng');
+            $table->decimal('product_price,10,2');
+            $table->text('product_desc');
+            $table->foreignId('product_category')->references('category_id')->on('product_categories');
+            $table->string('product_img')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
